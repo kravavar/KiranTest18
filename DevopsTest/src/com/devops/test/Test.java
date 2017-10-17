ad9fd4dc084753c460b5b6531f2d225d131788de
kravavar <32719993+kravavar@users.noreply.github.com>
2017-10-18 00:33:22 +0530

    Update Test.java
    second commit changes

--- DevopsTest/src/com/devops/test/Test.java
+++ DevopsTest/src/com/devops/test/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HHEEEEEEEEEEEEEEEEEEEEEELLOOOOOOOOOOOOOOOOOO Hey hey hey this is first program");
+		System.out.println("Second commit to test jenkins");
 
 	}
 
